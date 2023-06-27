cdb053db7e846db5689d21c8303f362c1540cee0
Charles Cruse <charle9@vt.edu>
2023-06-27 16:36:12 -0400

    added comments to Listings-index

--- frontend/src/store/components/Listings/index.js
+++ frontend/src/store/components/Listings/index.js
@@ -1,25 +1,32 @@
-import { fetchListings, getListings } from "../../listings"
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
+import { fetchListings, getListings } from "../../listings";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import Navigation from "../Navigation";
 import Carousel from "../Carousel";
-import ListingIndexItem from "./ListingIndexItem"
-import "./Listings.css"
+import ListingIndexItem from "./ListingIndexItem";
+import "./Listings.css";
 
 function ListingsIndex() {
 
+    //Acquiring all listings
     const dispatch = useDispatch();
-    const listings = useSelector(getListings)
+    const listings = useSelector(getListings);
     
+    //Rendering Listings
     useEffect(() => {
-        dispatch(fetchListings())
-    }, [dispatch])
+        dispatch(fetchListings());
+    }, [dispatch]);
 
 
     return (
         <>
+            {/* Navigation bar with links and searchbar */}
             <Navigation />
+
+            {/* Carousel to be added for filtering */}
             {/* <Carousel /> */}
+
+            {/* Displaying all listings */}
             <div id='listings-grid'>
                 {listings.map((listing) => {
                     return <ListingIndexItem listing={listing} key={listing.id} />
