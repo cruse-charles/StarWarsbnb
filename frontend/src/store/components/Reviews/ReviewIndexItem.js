1873b0fbd15bb905e17093f1b4b71fa9cba617e5
Charles Cruse <charle9@vt.edu>
2023-06-27 19:11:38 -0400

    added comments to ReviewIndexItem

--- frontend/src/store/components/Reviews/ReviewIndexItem.js
+++ frontend/src/store/components/Reviews/ReviewIndexItem.js
@@ -4,33 +4,41 @@ import { useDispatch, useSelector } from "react-redux";
 
 const ReviewIndexItem = ({review}) => {
     
-    const {listingId} = useParams()
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const user = useSelector(state => state.session.user)
+    const {listingId} = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector(state => state.session.user);
     
+    //Deletion of Review
     function handleDelete(e) {
-        e.preventDefault()
-        dispatch(deleteReview(review.id))
-        history.push(`/listings/${listingId}`)
+        e.preventDefault();
+        dispatch(deleteReview(review.id));
+        history.push(`/listings/${listingId}`);
     }
 
+    //Check if user is the same as the reviewer
     const checkUser = () => {
-        if(!user) return false
+        if(!user) return false;
         if(user.id === review.reviewerId) {
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
     }
 
+    
     return (
         <div className='review-card'>
+            {/* Review Details */}
             <div className='reviewer-name'>
                 <h2>{review.reviewerName}</h2>
             </div>
-            <div>{review.body}</div><br></br>
+            <div>
+                {review.body}
+            </div>
+            <br></br>
             
+            {/* Render editing/deleting functionality if userId matches */}
             {checkUser() && (<div id='edit-remove-container'>
                 <Link id='edit' to={`/listings/${listingId}/reviews/${review.id}/edit`}>
                     Edit your review
