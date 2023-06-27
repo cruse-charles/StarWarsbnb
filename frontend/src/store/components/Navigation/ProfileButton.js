2efc66bcb553c69391842315f2f0e669b8cc0a75
Charles Cruse <charle9@vt.edu>
2023-06-27 18:47:56 -0400

    added comments to ProfileButton

--- frontend/src/store/components/Navigation/ProfileButton.js
+++ frontend/src/store/components/Navigation/ProfileButton.js
@@ -9,53 +9,50 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function ProfileButton() {
   const dispatch = useDispatch();
-  // const [showMenu, setShowMenu] = useState(false);
   const {menuState, setMenuState} = useContext(menuContext) 
   const sessionUser = useSelector(state => state.session.user);
   const history = useHistory()
   
   const userId = useSelector(state => state.session?.user?.id)
 
-  
+  // Toggle modal state
   const toggleMenu = () => {
-    // setShowMenu(!showMenu);
     setMenuState(!menuState)
   };
 
+  //logout user
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
-    toggleMenu()
+    toggleMenu();
   };
 
+  //redirect to user's profile
   const routeChange = () => {
     if(userId){
-      let path = `/users/${userId}`
-      history.push(path)
+      let path = `/users/${userId}`;
+      history.push(path);
     }
   }
 
+  //Buttons for signup/login if there is no user
   let sessionLinks
   if(!sessionUser) {
     sessionLinks = (
       <>
-        {/* <div id='login-signup-menu'> */}
           <div id='login-button'><LoginFormModal /></div>
-          {/* <div onClick={toggleMenu} id='login-button'><LoginFormModal /></div> */}
           <div id='signup-button'><SignupFormModal /></div>
-        {/* </div> */}
       </>
     );
   }
 
+  //Buttons to see profile and logout if there is a user
   let userButtons
   if(sessionUser) {
     userButtons = (
       <>
       <div>
         <div onClick={routeChange} id='trips-button'>Trips</div><br></br>
-        {/* <div>Trips</div><br></br> */}
-        {/* <div id='wishlists-button'>Wishlists</div><br></br> */}
         <div onClick={logout} id='logout-button'>Log Out</div>
       </div>
       </>
@@ -69,14 +66,12 @@ function ProfileButton() {
         <i id='hamburger' className="fa-sharp fa-solid fa-bars fa-lg" />
         <i id='account-icon' className="fa-solid fa-user fa-lg" />
       </button>
+
+      {/* Seleccting either login/sign up buttons or profile/logout buttons */}
       {menuState && (
         <div id="profile-dropdown">
-          {/* <div> */}
             {sessionLinks}
-          {/* </div> */}
-          {/* <div> */}
             {userButtons}
-          {/* </div> */}
         </div>
       )}
     </>
