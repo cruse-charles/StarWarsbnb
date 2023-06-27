b62529724f6778543f0f576dea3cb6e875912225
Charles Cruse <charle9@vt.edu>
2023-06-27 16:32:17 -0400

    added comments to ReservationIndexItem

--- frontend/src/store/components/Reservations/ReservationIndexItem.js
+++ frontend/src/store/components/Reservations/ReservationIndexItem.js
@@ -1,43 +1,41 @@
-import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min"
-import { deleteReservation } from "../../reservations"
-import { useDispatch } from "react-redux"
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { deleteReservation } from "../../reservations";
+import { useDispatch } from "react-redux";
 
 const ReservationIndexItem = ({reservation}) => {
-    const dispatch = useDispatch()
-    const {userId} = useParams()
+    const dispatch = useDispatch();
+    const {userId} = useParams();
     
-
+    //Deleting a reservation
     function handleDelete(e) {
-        e.preventDefault()
-        dispatch(deleteReservation(reservation.id))
+        e.preventDefault();
+        dispatch(deleteReservation(reservation.id));
     }
 
+    //Adding one day to dates due to receiving a late timezone from data
     function addDays(date, days) {
         date.setDate(date.getDate() + days);
-        return date
+        return date;
     }
 
-    const beginDate = new Date(reservation.startDate)
-    const newBeginDate = addDays(beginDate, 1)
-    const formattedStartDate = newBeginDate.toDateString()
-
-
-    // console.log('reservation Start Date')
-    // console.log(reservation.startDate)
-    // console.log('beginDate')
-    // console.log(beginDate)
-    // console.log('formattedStartDate')
-    // console.log(formattedStartDate)
+    //Formatting start dates to make reservations
+    const beginDate = new Date(reservation.startDate);
+    const newBeginDate = addDays(beginDate, 1);
+    const formattedStartDate = newBeginDate.toDateString();
     
-    const endingDate = new Date(reservation.endDate)
-    const newEndingDate = addDays(endingDate, 1)    
-    const formattedEndDate = newEndingDate.toDateString()
+    //Formatting end dates to make reservations
+    const endingDate = new Date(reservation.endDate);
+    const newEndingDate = addDays(endingDate, 1);
+    const formattedEndDate = newEndingDate.toDateString();
 
     return(
         <div id='reservation-card'>
+            {/* Photo for listing */}
             <div id='photo-container'>
                 <img id='reservation-photo' src={reservation.listingPhoto?.[0]}></img>
             </div>
+
+            {/* Grabbing reservation information to render */}
             <div id='reservation-card-information-wrapper'>
                 <div id='information-container'>
                     <div id='reservastion-listing-title'><h1>{reservation.listingTitle}</h1></div>
@@ -47,9 +45,6 @@ const ReservationIndexItem = ({reservation}) => {
                         <Link id='remove' onClick={handleDelete}>Delete your reservation</Link>
                     </div>
                 </div>
-                {/* <div id='description-container'>
-                    {reservation.listingDescription}
-                </div> */}
             </div>
         </div>
     )
