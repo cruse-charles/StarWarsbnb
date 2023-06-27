32610f3ce10cf202049e588ad19b76f35a4b1b24
Charles Cruse <charle9@vt.edu>
2023-06-27 18:51:15 -0400

    added comments to SearchBar

--- frontend/src/store/components/Navigation/SearchBar.js
+++ frontend/src/store/components/Navigation/SearchBar.js
@@ -1,46 +1,44 @@
-import { useDispatch } from "react-redux"
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
-import { useEffect, useState } from "react"
-import { fetchSearchResults } from "../../search"
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useEffect, useState } from "react";
+import { fetchSearchResults } from "../../search";
 
 const SearchBar = () => {
 
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const [searchText, setSearchText] = useState("")
-//added for stuff
-    const [query, setQuery] = useState("")
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [searchText, setSearchText] = useState("");
+    const [query, setQuery] = useState("");
 
+    //Made to stop auto dropdown for search
     useEffect(() => {
 
-    }, [])
-//made this just now for stopping auto dropdown for search
+    }, []);
+    
 
-    //made to be async in case user is typing quickly and just happens to mess something up
+    //made to be async in case user is typing quickly
     async function handleSearch(e) {
-        e.preventDefault()
+        e.preventDefault();
         const query = e.target.value;
-        // query = e.target.value;
-        await setSearchText(query)
-        setQuery(query)
-        // dispatch(fetchSearchResults(query))
+        await setSearchText(query);
+        setQuery(query);
+        // dispatch(fetchSearchResults(query));
     }
 
     function handleSearchSubmit(e) {
-        e.preventDefault()
-        // if (searchText.length > 0) {
+        e.preventDefault();
+        // For autosearch: if (searchText.length > 0) {
         if (query.length > 0) {
-            history.push(`/search?listings=${query}`)
-            dispatch(fetchSearchResults(query))
+            history.push(`/search?listings=${query}`);
+            dispatch(fetchSearchResults(query));
         }
     }
 
     return (
         <>
             <input id='searchbar' type='text' onChange={handleSearch} placeholder='Anywhere | Anyweek | Add Guests'></input>
-            {/* this onChange lets us do a search for everything we type for the dropdown */}
+            {/* This onChange allows for a search of everything typed for dropdown */}
 
-            {/* <input id='searchbar' type='text' placeholder='Anywhere | Anyweek | Add Guests'></input> */}
             <button id='search-button' onClick={handleSearchSubmit}><i className="fa-solid fa-magnifying-glass fa-lg"></i></button>
         </>
     )
