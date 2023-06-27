d852f025cdf66ba5f1572660e143a4f9889a2da6
Charles Cruse <charle9@vt.edu>
2023-06-27 18:56:58 -0400

    added comments to Reviews.index

--- frontend/src/store/components/Reviews/index.js
+++ frontend/src/store/components/Reviews/index.js
@@ -1,23 +1,22 @@
-import { useDispatch, useSelector } from "react-redux"
-import { fetchReviews, getListingReviews } from "../../reviews"
-import { useParams } from 'react-router-dom'
-import { useEffect } from "react"
-import ReviewIndexItem from './ReviewIndexItem'
+import { useDispatch, useSelector } from "react-redux";
+import { fetchReviews, getListingReviews } from "../../reviews";
+import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import ReviewIndexItem from './ReviewIndexItem';
 
 
 
 const ListingReviews = () => {
-    const dispatch = useDispatch()
-    const {listingId} = useParams()
-    const reviews = useSelector(getListingReviews(listingId))
+    const dispatch = useDispatch();
+    const {listingId} = useParams();
+    const reviews = useSelector(getListingReviews(listingId));
     
-
+    //Fetch all reviews
     useEffect(() => {
-        
-        dispatch(fetchReviews(listingId))
-    }, [listingId, dispatch])
+        dispatch(fetchReviews(listingId));
+    }, [listingId, dispatch]);
 
-    
+    //If no reviews exist, return null
     if(!reviews) {
         return null;
     }
