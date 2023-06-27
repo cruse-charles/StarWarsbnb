9216908739af3b59fb1e38352ca3a5c4b05335c4
Charles Cruse <charle9@vt.edu>
2023-06-27 19:16:43 -0400

    added comments to SearchShowPage

--- frontend/src/store/components/SearchShowPage/SearchShowPage.js
+++ frontend/src/store/components/SearchShowPage/SearchShowPage.js
@@ -1,46 +1,40 @@
 import { useSelector } from 'react-redux';
-// import { fetchSearchResults } from '../../store/search';
 import { fetchSearchResults } from '../../search';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import Navigation from '../Navigation';
 import ListingIndexItem from '../Listings';
-import './SearchShowPage.css'
+import './SearchShowPage.css';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import testPhoto from '../../../../src/assets/l2p1.png'
+import testPhoto from '../../../../src/assets/l2p1.png';
 
 const Search = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-
-    // debugger;
     
+    // dispatch query when a new search is done
     useEffect(() => {
         const query = history.location.search.split('=')[1];
-        dispatch(fetchSearchResults(query))
+        dispatch(fetchSearchResults(query));
     }, []);
     
     const searchResults = useSelector((state) => state.searchResults );
-    // debugger
-
-    // const routeChange = () => {
-    //     let path = `/listings/${searchResults.id}`
-    //     history.push(path)
-    // }
 
 
     return(
         <>
         <Navigation />
         <div id='listings-grid'>
+            {/* Map search results */}
             {Object.values(searchResults).map((ele) => {
                 return (
                     <a href={`/listings/${ele.id}`}>
-                        {/* <div id='listing-card' onClick={routeChange}> */}
                         <div id='listing-card'>
+                            {/* Test photo kept for when adjustments are made to site to ensure no overage on AWS */}
                             <img id='listing-profile-photo'src={ele?.photoUrls?.[0]} alt='listing'/>
                             {/* <img id='listing-profile-photo'src={testPhoto} alt='listing'/> */}
+
                             <div id='listing-city-country'>{ele.city}, {ele.country}</div>
                             <div>Hosted by a superhost!</div>
                             <div>Apr 15- Apr 18</div>
