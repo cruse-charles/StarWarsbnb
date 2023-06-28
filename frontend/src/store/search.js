48f653c920b81ca440f4e6495901473114a52176
Charles Cruse <charle9@vt.edu>
2023-06-27 23:33:02 -0400

    added comments to search.js

--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -1,26 +1,27 @@
+//Action types to receive search results
 export const GET_SEARCH_RESULTS = 'search/searchResults';
 
-
+//Action Creator, returns action object for search
 export const receiveSearchResults = searchResults => ({
     type: GET_SEARCH_RESULTS,
     searchResults
 });
 
-
+//Thunk action creator to fetch listings from API and add to our state/store
 export const fetchSearchResults = (query) => async dispatch => {
     const res = await fetch(`/api/listings/search?query=${query}`);
     const data = await res.json();
     dispatch(receiveSearchResults(data));
 };
 
-
+//Automatically called by Redux whenever an action is dispatched for obtaining listings or listing
 const searchReducer = (state = {}, action) => {
     switch (action.type) {
         case GET_SEARCH_RESULTS:
             if(action.searchResults.search){
-                return action.searchResults.search
+                return action.searchResults.search;
             } else{
-                return {}
+                return {};
             }
         default:
             return state;
