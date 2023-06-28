aa0df21ac9be978275dc13d8125a2b760d36e655
Charles Cruse <charle9@vt.edu>
2023-06-27 21:19:35 -0400

    added comments to reviews_controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,28 +1,29 @@
 class Api::ReviewsController < ApplicationController
   before_action :require_logged_in, only: [:create, :update, :destroy]
 
+  #Pulling all reviews based on listing ID
   def index
       @reviews =  Review.where(listing_id: params[:listing_id])
       render :index
   end
 
+  #Showing specific review based on review ID
   def show
     @review = Review.find_by(id: params[:id])
     render :show
-    # render json: {code: "success"}
   end
 
+  #Creating a review
   def create
     @review = Review.new(review_params)
     if @review.save
       render :show
     else
       render json: @review.errors.full_messages
-      # return errors.add(:error, 'You must write a comment')
-      # render json: { errors: ['The provided credentials were invalid.'] }
     end
   end
 
+  #Updating a review found by review ID, and checking reviewer/current user ID
   def update
     @review = Review.find_by(id: params[:id])
     
@@ -37,12 +38,11 @@ class Api::ReviewsController < ApplicationController
     end
   end
 
+  #Deleting a review found by ID
   def destroy
     @review = Review.find_by(id: params[:id])
     if @review.reviewer_id == current_user.id
       @review.destroy
-      # render json: { message: 'Deleted review'}
-      # render :show
     else
       render json: {errors: 'Must be owner of this review to delete'}
     end
