c4b1408b3758e80f35eb1831ff2db5074cc7d1f6
Charles Cruse <charle9@vt.edu>
2023-06-28 20:19:48 -0400

    added map to splash page with toggle

--- frontend/src/store/components/Listings/index.js
+++ frontend/src/store/components/Listings/index.js
@@ -1,6 +1,6 @@
 import { fetchListings, getListings } from "../../listings";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navigation from "../Navigation";
 import Carousel from "../Carousel";
 import ListingIndexItem from "./ListingIndexItem";
@@ -12,29 +12,51 @@ function ListingsIndex() {
     //Acquiring all listings
     const dispatch = useDispatch();
     const listings = useSelector(getListings);
+    const [showMap, setShowMap] = useState(false)
     
     //Rendering Listings
     useEffect(() => {
         dispatch(fetchListings());
     }, [dispatch]);
 
+    const toggleMap = () => {
+        setShowMap(!showMap)
+    }
 
     return (
         <>
+        <div id='listings-page-wrapper'>
             {/* Navigation bar with links and searchbar */}
             <Navigation />
 
             {/* Carousel to be added for filtering */}
             {/* <Carousel /> */}
 
+            {showMap ? (
+                <div id='listings-map'>
+                    <Map />
+                </div>
+            ) : (
+                <div id='listings-grid'>
+                    {listings.map((listing) => {
+                        return <ListingIndexItem listing={listing} key={listing.id} />
+                    })}
+                </div>
+            )}
             {/* Displaying all listings */}
-            <div id='listings-grid'>
-                {listings.map((listing) => {
-                    return <ListingIndexItem listing={listing} key={listing.id} />
-                })}
-            </div>
 
-            <Map />
+            <button id="show-map-button" onClick={toggleMap}>
+                {showMap ? (
+                    <>
+                        Show List <i class="fa-solid fa-list"></i> 
+                    </>
+                ) : (
+                    <>
+                    Show Map <i class="fa-solid fa-map"></i> 
+                    </>
+                )}
+            </button>
+        </div>
         </>
     )
 }
