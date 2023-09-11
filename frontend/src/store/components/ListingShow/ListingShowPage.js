b9ee54fd78f7f73632c83b4479160973d7cfd988
Charles Cruse <charle9@vt.edu>
2023-09-10 22:39:06 -0400

    refactored update review code

--- frontend/src/store/components/ListingShow/ListingShowPage.js
+++ frontend/src/store/components/ListingShow/ListingShowPage.js
@@ -1,148 +1,203 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom/cjs/react-router-dom.min";
 import { fetchListing, getListing } from "../../listings";
 import { useEffect } from "react";
 import Navigation from "../Navigation";
-import './ListingShowPage.css'
-import testPhoto from '../../../../src/assets/l2p1.png'
+import "./ListingShowPage.css";
+import testPhoto from "../../../../src/assets/l2p1.png";
 import ListingReviews from "../Reviews";
 import ListsingReservationForm from "../Reservations/ListingReservationForm";
 
 const ListingShowPage = () => {
-    const dispatch = useDispatch();
-    const {listingId} = useParams();
+  const dispatch = useDispatch();
+  const { listingId } = useParams();
 
-    //This should match with the url wildcard in the APP route, to always receive the correct one
-    const listing = useSelector(getListing(listingId));
-    let user = useSelector((state) => (state.session?.user));
+  //This should match with the url wildcard in the APP route, to always receive the correct one
+  const listing = useSelector(getListing(listingId));
+  let user = useSelector((state) => state.session?.user);
 
+  useEffect(() => {
+    dispatch(fetchListing(listingId));
+  }, [listingId, dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchListing(listingId));
-    }, [listingId, dispatch]);
+  //Should not display if there is no listing in state
+  if (!listing) {
+    return null;
+  }
 
-    //Should not display if there is no listing in state
-    if(!listing) {
-        return null;
-    };
+  return (
+    <>
+      <div id="listings-page">
+        {/* Navigation bar including links and search bar and sign-in */}
+        <Navigation />
 
-    return (
-        <>
-        <div id='listings-page'>
-            {/* Navigation bar including links and search bar and sign-in */}
-            <Navigation />
+        {/* Titles for listing page */}
+        <div id="title-container">
+          <h1 id="title">{listing.title}</h1>
+          <br></br>
+          <div id="location">
+            {listing.city}, {listing.country}
+          </div>
+        </div>
 
-            {/* Titles for listing page */}
-            <div id='title-container'>
-                <h1 id='title'>{listing.title}</h1><br></br>
-                <div id='location'>{listing.city}, {listing.country}</div>
-            </div>
-
-            {/* Photos of listing, commented out portions are kept for testing for website adjustment to not exceed AWS limit */}
-            <div id='images-wrapper'>
-                <div id='images-container'>
-                    <img className='main-img'src={listing?.photoUrls?.[0]} alt='listing'/>
-                    <img className='small-img' src={listing?.photoUrls?.[1]} alt='listing'/>
-                    <img className='small-img' src={listing?.photoUrls?.[2]} alt='listing'/>
-                    <img id='top-right-pic' className='small-img' src={listing?.photoUrls?.[3]} alt='listing'/>
-                    <img id='bottom-right-pic'className='small-img' src={listing?.photoUrls?.[4]} alt='listing'/>
-                    {/* <img className='main-img'src={testPhoto} alt='listing'/>
+        {/* Photos of listing, commented out portions are kept for testing for website adjustment to not exceed AWS limit */}
+        <div id="images-wrapper">
+          <div id="images-container">
+            <img
+              className="main-img"
+              src={listing?.photoUrls?.[0]}
+              alt="listing"
+            />
+            <img
+              className="small-img"
+              src={listing?.photoUrls?.[1]}
+              alt="listing"
+            />
+            <img
+              className="small-img"
+              src={listing?.photoUrls?.[2]}
+              alt="listing"
+            />
+            <img
+              id="top-right-pic"
+              className="small-img"
+              src={listing?.photoUrls?.[3]}
+              alt="listing"
+            />
+            <img
+              id="bottom-right-pic"
+              className="small-img"
+              src={listing?.photoUrls?.[4]}
+              alt="listing"
+            />
+            {/* <img className='main-img'src={testPhoto} alt='listing'/>
                     <img className='small-img' src={testPhoto} alt='listing'/>
                     <img className='small-img' src={testPhoto} alt='listing'/>
                     <img id='top-right-pic' className='small-img' src={testPhoto} alt='listing'/>
                     <img id='bottom-right-pic'className='small-img' src={testPhoto} alt='listing'/> */}
-                </div>
-            </div>
-
-            {/* 'Highlights' for listing */}
-            <div id='bottom-portion'>
-                <div id='information-wrapper'>
-                    <div id='information-container'>                        
-                        <div id='hosted-by-container'>
-                            <h1>Entire home hosted by {listing.host}</h1>
-                            <h2>4 guests 2 bedrooms 3 beds 1.5 baths</h2>
-                        </div>
-                        <div id='highlights-wrapper'>
-                            <div className='highlight-container' id='highlight3'>
-                                <div className='highlight-icon-container'><i className="fa-solid fa-medal fa-xl"></i></div>
-                                <div className='highlight-text-container'>
-                                    <h2>This host is a SuperHost</h2>
-                                    <div>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</div>
-                                </div>
-                            </div>
-                            <div className='highlight-container' id='highlight3'>
-                                <div className='highlight-icon-container'><i className="fa-solid fa-location-dot fa-xl"></i></div>
-                                <div className='highlight-text-container'>
-                                    <h2>Great location</h2>
-                                    <div>95% of recent guests gave the location a 5-star rating.</div>
-                                </div>
-                            </div>
-                            <div className='highlight-container' id='highlight3'>
-                                <div className='highlight-icon-container'><i className="fa-solid fa-calendar fa-xl"></i></div>
-                                <div className='highlight-text-container'>
-                                    <h2>Free cancellation within 48 hours.</h2>
-                                    <div></div>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* Listing's description pulled from database and properly formatted */}
-                        <div id='description-container'>
-                            {/* <h2>{listing.description}</h2> */}
-                            <div id="lsp_listing_description">
-                                {listing.description.split("\n").map((line) => <h2>{line}<br></br></h2>)}
-                            </div>
-                        </div>
-
-                        {/* Icons for what is offered at listing */}
-                        <div id='icons-container'>
-                            <h2>What this place offers</h2>
-                            <div id='icons-list'>
-                                <div className='offered-icons'>
-                                    <i className="fa-solid fa-kitchen-set fa-lg"></i>
-                                    <p>Kitchen</p>
-                                </div>
-                                <div className='offered-icons'><i className="fa-solid fa-wifi fa-lg"></i>Wifi</div>
-                                <div className='offered-icons'><i className="fa-solid fa-bath fa-xl"></i>Bathtub</div>
-                                <div className='offered-icons'><i className="fa-solid fa-hot-tub-person fa-lg"></i>Hottub</div>
-                                <div className='offered-icons'><i className="fa-solid fa-water-ladder fa-lg"></i>Pool</div>
-                                <div className='offered-icons'><i className="fa-solid fa-tv fa-lg"></i>TV</div>
-                                <div className='offered-icons'><i className="fa-solid fa-paw fa-xl"></i>Pets Allowed</div>
-                                <div className='offered-icons'><i className="fa-solid fa-car fa-lg"></i>Parking</div>
-                                <div className='offered-icons'><i className="fa-solid fa-snowflake fa-lg"></i>Air Conditioning</div>
-                                
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Calendar to create reservations for this listing */}
-                    <ListsingReservationForm />
-                </div>
-
-                {/* Writing and Viewing Reviews */}
-                <div id='reviews-wrapper'>
-                    <div id='review-stats'>
-
-                        {/* if signed in, can write a rewview */}
-                        { user &&
-                            <Link id='write-review' to={`/listings/${listingId}/reviews/new`}>Click here to write a review!</Link>
-                        }
-
-                        {/* Listing's reviews */}
-                        <h2>Reviews for this airbnb</h2>
-                        <ListingReviews />
-                    </div>
-                    <div id='reviews-container'>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        </>
-    )
 
+        {/* 'Highlights' for listing */}
+        <div id="bottom-portion">
+          <div id="information-wrapper">
+            <div id="information-container">
+              <div id="hosted-by-container">
+                <h1>Entire home hosted by {listing.host}</h1>
+                <h2>4 guests 2 bedrooms 3 beds 1.5 baths</h2>
+              </div>
+              <div id="highlights-wrapper">
+                <div className="highlight-container" id="highlight3">
+                  <div className="highlight-icon-container">
+                    <i className="fa-solid fa-medal fa-xl"></i>
+                  </div>
+                  <div className="highlight-text-container">
+                    <h2>This host is a SuperHost</h2>
+                    <div>
+                      Superhosts are experienced, highly rated hosts who are
+                      committed to providing great stays for guests.
+                    </div>
+                  </div>
+                </div>
+                <div className="highlight-container" id="highlight3">
+                  <div className="highlight-icon-container">
+                    <i className="fa-solid fa-location-dot fa-xl"></i>
+                  </div>
+                  <div className="highlight-text-container">
+                    <h2>Great location</h2>
+                    <div>
+                      95% of recent guests gave the location a 5-star rating.
+                    </div>
+                  </div>
+                </div>
+                <div className="highlight-container" id="highlight3">
+                  <div className="highlight-icon-container">
+                    <i className="fa-solid fa-calendar fa-xl"></i>
+                  </div>
+                  <div className="highlight-text-container">
+                    <h2>Free cancellation within 48 hours.</h2>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
 
+              {/* Listing's description pulled from database and properly formatted */}
+              <div id="description-container">
+                {/* <h2>{listing.description}</h2> */}
+                <div id="lsp_listing_description">
+                  {listing.description.split("\n").map((line, index) => (
+                    <h2 key={index}>
+                      {line}
+                      <br></br>
+                    </h2>
+                  ))}
+                </div>
+              </div>
 
-}
+              {/* Icons for what is offered at listing */}
+              <div id="icons-container">
+                <h2>What this place offers</h2>
+                <div id="icons-list">
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-kitchen-set fa-lg"></i>
+                    <p>Kitchen</p>
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-wifi fa-lg"></i>Wifi
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-bath fa-xl"></i>Bathtub
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-hot-tub-person fa-lg"></i>Hottub
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-water-ladder fa-lg"></i>Pool
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-tv fa-lg"></i>TV
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-paw fa-xl"></i>Pets Allowed
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-car fa-lg"></i>Parking
+                  </div>
+                  <div className="offered-icons">
+                    <i className="fa-solid fa-snowflake fa-lg"></i>Air
+                    Conditioning
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            {/* Calendar to create reservations for this listing */}
+            <ListsingReservationForm />
+          </div>
 
-export default ListingShowPage
+          {/* Writing and Viewing Reviews */}
+          <div id="reviews-wrapper">
+            <div id="review-stats">
+              {/* if signed in, can write a rewview */}
+              {user && (
+                <Link
+                  id="write-review"
+                  to={`/listings/${listingId}/reviews/new`}
+                >
+                  Click here to write a review!
+                </Link>
+              )}
+
+              {/* Listing's reviews */}
+              <h2>Reviews for this airbnb</h2>
+              <ListingReviews />
+            </div>
+            <div id="reviews-container"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingShowPage;
--- frontend/src/store/components/Reviews/ReviewForm.js
+++ frontend/src/store/components/Reviews/ReviewForm.js
@@ -1,144 +1,162 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useParams,
+  useHistory,
+} from "react-router-dom/cjs/react-router-dom.min";
 import { useEffect, useState } from "react";
-import { updateReview, createReview, fetchReview, getReview } from '../../reviews';
-import './ReviewForm.css';
+import {
+  updateReview,
+  createReview,
+  fetchReview,
+  getReview,
+} from "../../reviews";
+import "./ReviewForm.css";
 
 const ReviewForm = () => {
-    const dispatch = useDispatch();
-    const {listingId, reviewId} = useParams();
-    let user = useSelector((state) => (state.session.user));
-    let review = useSelector(getReview(reviewId));
-    const history = useHistory();
-    const [errors, setErrors] = useState([]);
-    const [header, setHeader] = useState('Write a review!');
-    const [body, setBody] = useState();
-    const [cleanliness, setCleanliness] = useState();
-    const [communication, setCommunication] = useState();
-    const [checkIn, setCheckIn] = useState();
-    const [accuracy, setAccuracy] = useState();
-    const [location, setLocation] = useState();
-    const [value, setValue] = useState();
-    const changeHandlers = {
-        "Cleanliness" : setCleanliness,
-        "Communication" : setCommunication,
-        "Check In" : setCheckIn,
-        "Accuracy" : setAccuracy,
-        "Location" : setLocation,
-        "Value" : setValue
+  const dispatch = useDispatch();
+  const { listingId, reviewId } = useParams();
+  let user = useSelector((state) => state.session.user);
+  let review = useSelector(getReview(reviewId));
+  const history = useHistory();
+  const [errors, setErrors] = useState([]);
+  const [header, setHeader] = useState("Write a review!");
+  const [body, setBody] = useState();
+  const [cleanliness, setCleanliness] = useState();
+  const [communication, setCommunication] = useState();
+  const [checkIn, setCheckIn] = useState();
+  const [accuracy, setAccuracy] = useState();
+  const [location, setLocation] = useState();
+  const [value, setValue] = useState();
+  const changeHandlers = {
+    Cleanliness: setCleanliness,
+    Communication: setCommunication,
+    "Check In": setCheckIn,
+    Accuracy: setAccuracy,
+    Location: setLocation,
+    Value: setValue,
+  };
+
+  //Redirect to listing after submitting review
+  const routeChange = () => {
+    let path = `/listings/${listingId}`;
+    history.push(path);
+  };
+
+  //Pull previous review's data if editng a review
+  useEffect(() => {
+    if (reviewId) {
+      dispatch(fetchReview(reviewId));
+      setHeader("Edit your review:");
+      setBody(review.body);
+      setCleanliness(review.cleanliness);
+      setCommunication(review.communication);
+      setCheckIn(review.checkIn);
+      setAccuracy(review.accuracy);
+      setLocation(review.location);
+      setValue(review.value);
+      setErrors([]);
+    }
+  }, [dispatch, reviewId]);
+
+  //Create 5 starred radio buttons per category
+  const createCategoryStars = (category, currentValue) => {
+    return (
+      <div id="individual-review-container">
+        <h2>{category}</h2>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <label className="fa-solid fa-star fa-xl" key={value}>
+            {value}
+            <input
+              required
+              className="star"
+              type="radio"
+              name={category}
+              value={value}
+              onChange={(e) => {
+                changeHandlers[category](e.target.value);
+              }}
+              checked={parseInt(currentValue) === value}
+            />
+          </label>
+        ))}
+      </div>
+    );
+  };
+
+  //Submit review
+  function handleSubmit(e) {
+    // const handleSubmit = (e) => {
+    e.preventDefault();
+    const newReview = {
+      body,
+      cleanliness,
+      communication,
+      check_in: checkIn,
+      accuracy,
+      location,
+      value,
     };
 
-    //Redirect to listing after submitting review
-    const routeChange = () => {
-        let path = `/listings/${listingId}`;
-        history.push(path);
+    newReview.listing_id = listingId;
+    newReview.reviewer_id = user.id;
+    if (reviewId) {
+      newReview.id = reviewId;
+      dispatch(updateReview(newReview))
+        .then(routeChange)
+        .catch(async (res) => {
+          // let data = await res[0]
+          let data = await res;
+          if (data) {
+            setErrors(data);
+          }
+        });
+    } else {
+      dispatch(createReview(newReview))
+        .then(routeChange)
+        .catch(async (res) => {
+          // let data = await res[0]
+          let data = await res;
+          if (data) {
+            setErrors(data);
+          }
+        });
     }
+  }
 
-    //Pull previous review's data if editng a review
-    useEffect(() => {
-        if(reviewId){
-            dispatch(fetchReview(reviewId));
-            setHeader('Edit your review:');
-            setBody(review.body);
-            setCleanliness(review.cleanliness);
-            setCommunication(review.communication);
-            setCheckIn(review.checkIn);
-            setAccuracy(review.accuracy);
-            setLocation(review.location);
-            setValue(review.value);
-            setErrors([]);
-        }
-    }, [dispatch, reviewId]);
+  //Changing input for review's for change in selection
+  function changeBody(e) {
+    setBody(e.target.value);
+  }
 
-    //Create 5 starred radio buttons per category
-    const createCategoryStars = (category) => {
-        return (
-            <div id='individual-review-container'>
-                <h2>{category}</h2>
-                <label className="fa-solid fa-star fa-xl">1 
-                    <input required className="star" type='radio' name={category} value='1' onChange={(e) => {changeHandlers[category](e.target.value)}}/>
-                </label>
-                <label className="fa-solid fa-star fa-xl">2 
-                    <input required className="star" type='radio' name={category} value='2' onChange={(e) => {changeHandlers[category](e.target.value)}}/>
-                </label>
-                <label className="fa-solid fa-star fa-xl">3 
-                    <input required className="star" type='radio' name={category} value='3' onChange={(e) => {changeHandlers[category](e.target.value)}}/>
-                </label>
-                <label className="fa-solid fa-star fa-xl">4 
-                    <input required className="star" type='radio' name={category} value='4' onChange={(e) => {changeHandlers[category](e.target.value)}}/>
-                </label>
-                <label className="fa-solid fa-star fa-xl">5 
-                    <input required className="star" type='radio' name={category} value='5' onChange={(e) => {changeHandlers[category](e.target.value)}}/>
-                </label>
-            </div>
-        )
-    }
-
-    //Submit review
-    function handleSubmit(e){
-    // const handleSubmit = (e) => {
-        e.preventDefault()
-        const newReview = {
-            body,
-            cleanliness,
-            communication,
-            check_in: checkIn,
-            accuracy,
-            location,
-            value
-        };
-
-        newReview.listing_id = listingId;
-        newReview.reviewer_id = user.id;
-        if(reviewId){
-            newReview.id = reviewId
-            dispatch(updateReview(newReview))
-            .then(routeChange)
-            .catch(async (res) => {
-                // let data = await res[0]
-                let data = await res
-                if (data){
-                    setErrors(data)
-                }
-            })
-        } else {
-            dispatch(createReview(newReview))
-            .then(routeChange)
-            .catch(async (res) => {
-                // let data = await res[0]
-                let data = await res
-                if (data){
-                    setErrors(data)
-                }
-            })
-        }
-    }
-
-    //Changing input for review's for change in selection
-    function changeBody(e){
-        setBody(e.target.value)
-    }
-
-    return (
-        <>
-        <form id='review-container'>
-            <h1>{header}</h1>
-            <ul id='rf-errors'>
-                {errors.map((error, i) => (<li key={i}>{error}</li>))}
-            </ul>
-            {createCategoryStars('Cleanliness')}
-            {createCategoryStars('Communication')}
-            {createCategoryStars('Check In')}
-            {createCategoryStars('Accuracy')}
-            {createCategoryStars('Location')}
-            {createCategoryStars('Value')}
-            <br></br>
-            <h2>Leave a comment...</h2>
-            <textarea required id='review-form-comment' value={body} onChange={changeBody}></textarea>
-            <br></br>
-            <button type='submit' id='review-form-button' onClick={handleSubmit} >Submit</button>
-        </form>
-        </>
-    )
-}
-export default ReviewForm
+  return (
+    <>
+      <form id="review-container">
+        <h1>{header}</h1>
+        <ul id="rf-errors">
+          {errors.map((error, i) => (
+            <li key={i}>{error}</li>
+          ))}
+        </ul>
+        {createCategoryStars("Cleanliness", cleanliness)}
+        {createCategoryStars("Communication", communication)}
+        {createCategoryStars("Check In", checkIn)}
+        {createCategoryStars("Accuracy", accuracy)}
+        {createCategoryStars("Location", location)}
+        {createCategoryStars("Value", value)}
+        <br></br>
+        <h2>Leave a comment...</h2>
+        <textarea
+          required
+          id="review-form-comment"
+          value={body}
+          onChange={changeBody}
+        ></textarea>
+        <br></br>
+        <button type="submit" id="review-form-button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+export default ReviewForm;
