22e4656699f4a995df4e838c66f29ceb708adc9c
Charles Cruse <charle9@vt.edu>
2023-10-23 23:51:23 -0400

    import navbar bootstrap

--- frontend/src/store/components/Listings/ListingIndexItem.js
+++ frontend/src/store/components/Listings/ListingIndexItem.js
@@ -1,97 +1,114 @@
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import React, { useState, useEffect }  from 'react';
-import testPhoto from '../../../../src/assets/l2p1.png';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState, useEffect } from "react";
+import testPhoto from "../../../../src/assets/l2p1.png";
+import { Card } from "react-bootstrap";
 
+const ListingIndexItem = ({ listing, routeChange, context }) => {
+  const history = useHistory();
+  const [showLeftArrow, setShowLeftArrow] = useState(false);
+  const [showRightArrow, setShowRightArrow] = useState(true);
+  const [activePhotoIndex, setActivePhotoIndex] = useState(0);
+  const [slideDirection, setSlideDirection] = useState("");
 
-const ListingIndexItem = ({listing, routeChange, context}) => {
-    const history = useHistory();
-    const [showLeftArrow, setShowLeftArrow] = useState(false)
-    const [showRightArrow, setShowRightArrow] = useState(true)
-    const [activePhotoIndex, setActivePhotoIndex] = useState(0)
-    const [slideDirection, setSlideDirection] = useState('');
-    
+  const handleClick = () => {
+    if (routeChange) {
+      routeChange(listing);
+    }
+  };
 
-    const handleClick = () => {
-        if (routeChange) {
-            routeChange(listing)
-        }
+  //Rendering buttons based on current position of photos array
+  useEffect(() => {
+    if (activePhotoIndex > 0) {
+      setShowLeftArrow(true);
+    } else {
+      setShowLeftArrow(false);
     }
 
-    //Rendering buttons based on current position of photos array
-    useEffect(() => {
-        if(activePhotoIndex > 0) {
-            setShowLeftArrow(true)
-        } else {
-            setShowLeftArrow(false)
-        }
+    if (activePhotoIndex == listing.photoUrls.length - 1) {
+      setShowRightArrow(false);
+    } else {
+      setShowRightArrow(true);
+    }
+  }, [activePhotoIndex]);
 
-        if (activePhotoIndex == listing.photoUrls.length - 1) {
-            setShowRightArrow(false)
-        } else {
-            setShowRightArrow(true)
-        }
-    }, [activePhotoIndex])
+  const handleLeftArrow = () => {
+    if (activePhotoIndex > 0) {
+      setSlideDirection("slide-out-right");
+      setTimeout(() => {
+        setActivePhotoIndex(activePhotoIndex - 1);
+        setSlideDirection("slide-in-left");
+      }, 100); // Adjust the transition duration as needed
+    }
+  };
 
-    const handleLeftArrow = () => {
-        if (activePhotoIndex > 0) {
-          setSlideDirection('slide-out-right');
-          setTimeout(() => {
-            setActivePhotoIndex(activePhotoIndex - 1);
-            setSlideDirection('slide-in-left');
-          }, 100); // Adjust the transition duration as needed
-        }
-      };
-      
-      const handleRightArrow = () => {
-        if (activePhotoIndex < listing.photoUrls.length - 1) {
-          setSlideDirection('slide-out-left');
-          setTimeout(() => {
-            setActivePhotoIndex(activePhotoIndex + 1);
-            setSlideDirection('slide-in-right');
-          }, 100); // Adjust the transition duration as needed
-        }
-      };
+  const handleRightArrow = () => {
+    if (activePhotoIndex < listing.photoUrls.length - 1) {
+      setSlideDirection("slide-out-left");
+      setTimeout(() => {
+        setActivePhotoIndex(activePhotoIndex + 1);
+        setSlideDirection("slide-in-right");
+      }, 100); // Adjust the transition duration as needed
+    }
+  };
 
-    return(
-        <div id='listing-card' className={context}>
-            {/* Test photo to be kept in for site adjustments and not overusing data */}
-            {/* <img id='listing-profile-photo'src={testPhoto} alt='listing' onClick={handleClick}/> */}
+  return (
+    //  THIS REQUIRES THAT I MAKE THE IMAGE A COMPONENT WITH A CAROSEL SINCE I MADE THE IMAGES CUSTOM BASICALLY
+    // <Card>
+    //     <Card.Img variant="top" src={listing?.photoUrls?.[activePhotoIndex]} />
+    //     <Card.Body>
+    //         <Card.Text>
 
-            <img
-                // key={activePhotoIndex}
-                id={`${context}-listing-profile-photo`}
-                className={`listing-photo ${slideDirection}`}
-                src={listing?.photoUrls?.[activePhotoIndex]}
-                alt="listing"
-                onClick={handleClick}
-            />
+    //         </Card.Text>
+    //     </Card.Body>
+    // </Card>
 
-            <div id={`${context}-picture-arrows-container`}>
-                {showLeftArrow && (
-                    <button id='left-arrow' className='picture-arrows' onClick={handleLeftArrow}>
-                        <i className="fa-solid fa-chevron-left"></i>
-                    </button>
-                )}
+    <div id="listing-card" className={context}>
+      {/* Test photo to be kept in for site adjustments and not overusing data */}
+      {/* <img id='listing-profile-photo'src={testPhoto} alt='listing' onClick={handleClick}/> */}
 
-                {showRightArrow && (
-                    <button id='right-arrow' className='picture-arrows' onClick={handleRightArrow}>
-                        <i className="fa-solid fa-chevron-right"></i>
-                    </button>
-                )}
+      <img
+        // key={activePhotoIndex}
+        id={`${context}-listing-profile-photo`}
+        className={`listing-photo ${slideDirection}`}
+        src={listing?.photoUrls?.[activePhotoIndex]}
+        alt="listing"
+        onClick={handleClick}
+      />
 
-            </div>
-            
-            <div id={`${context}-listing-card-info-container`} onClick={handleClick}>
-                <div id={`${context}-listing-city-country`}>{listing.city}, {listing.country}</div>
-                <div id={`${context}-hosted-by-line`}>Hosted by a superhost!</div>
-                <div id={`${context}-date-line`}>Aug 15 - Aug 18</div>
-                <div id={`${context}-listing-price-line`}><span id='listing-price'>${listing.price}</span> night</div>
-            </div>
+      <div id={`${context}-picture-arrows-container`}>
+        {showLeftArrow && (
+          <button
+            id="left-arrow"
+            className="picture-arrows"
+            onClick={handleLeftArrow}
+          >
+            <i className="fa-solid fa-chevron-left"></i>
+          </button>
+        )}
+
+        {showRightArrow && (
+          <button
+            id="right-arrow"
+            className="picture-arrows"
+            onClick={handleRightArrow}
+          >
+            <i className="fa-solid fa-chevron-right"></i>
+          </button>
+        )}
+      </div>
+
+      <div id={`${context}-listing-card-info-container`} onClick={handleClick}>
+        <div id={`${context}-listing-city-country`}>
+          {listing.city}, {listing.country}
         </div>
-    )
-}
+        <div id={`${context}-hosted-by-line`}>Hosted by a superhost!</div>
+        <div id={`${context}-date-line`}>Aug 15 - Aug 18</div>
+        <div id={`${context}-listing-price-line`}>
+          <span id="listing-price">${listing.price}</span> night
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ListingIndexItem
-
-
-
+export default ListingIndexItem;
--- frontend/src/store/components/Navigation/index.js
+++ frontend/src/store/components/Navigation/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import airbnbLogo from '../../../../src/assets/airbnb-logo.png';
-import linkedInLogo from '../../../../src/assets/linkedin-logo.png';
-import githubLogo from '../../../../src/assets/githubicon.png';
-import { createContext, useState } from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import SearchBar from './SearchBar';
+import React from "react";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import airbnbLogo from "../../../../src/assets/airbnb-logo.png";
+import linkedInLogo from "../../../../src/assets/linkedin-logo.png";
+import githubLogo from "../../../../src/assets/githubicon.png";
+import { createContext, useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import SearchBar from "./SearchBar";
+import { Navbar } from "react-bootstrap";
 
 export const menuContext = createContext();
 
@@ -14,43 +15,41 @@ const MenuContextProvider = (props) => {
   const [menuState, setMenuState] = useState(false);
 
   return (
-    <menuContext.Provider value={{menuState, setMenuState}}>
+    <menuContext.Provider value={{ menuState, setMenuState }}>
       {props.children}
     </menuContext.Provider>
-  )
+  );
+};
 
-}
-
-function Navigation(){
+function Navigation() {
   return (
     <MenuContextProvider>
-    <div id='navigation-wrapper'>
-      <div className='navigation-bar'>
+      <div id="navigation-wrapper">
+        <div className="navigation-bar">
+          {/* Section for links to other sits */}
+          <div className="airbnb-logo">
+            <Link to={"/"}>
+              <img id="airbnb-logo" src={airbnbLogo} alt="logo" />
+            </Link>
+            <a href="https://www.linkedin.com/in/charles-cruse-2ba72ab6/">
+              <img id="linkedin-logo" src={linkedInLogo} />
+            </a>
+            <a href="https://github.com/cruse-charles/StarWarsbnb">
+              <img id="github-logo" src={githubLogo} />
+            </a>
+          </div>
 
-        {/* Section for links to other sits */}
-        <div className='airbnb-logo'>
-          <Link to={'/'}>
-            <img id='airbnb-logo' src={airbnbLogo} alt='logo'/>
-          </Link>
-          <a href='https://www.linkedin.com/in/charles-cruse-2ba72ab6/'>
-            <img id='linkedin-logo' src={linkedInLogo}/>
-          </a>
-          <a href='https://github.com/cruse-charles/StarWarsbnb'>
-            <img id='github-logo' src={githubLogo}/>
-          </a>
-        </div>
+          {/* Searchbar */}
+          <div id="searchbar-container">
+            <SearchBar />
+          </div>
 
-        {/* Searchbar */}
-        <div id='searchbar-container'>
-          <SearchBar />
-        </div>
-
-        {/* Login/Signup */}
-        <div id='profile-button-container'>
-          <ProfileButton />
+          {/* Login/Signup */}
+          <div id="profile-button-container">
+            <ProfileButton />
+          </div>
         </div>
       </div>
-    </div>
     </MenuContextProvider>
   );
 }
